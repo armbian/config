8dc96878ae43f79cd00d89d481ba69ec966f8110
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2014-11-09 09:47:49 +0100

    typo

--- install.sh
+++ install.sh
@@ -98,4 +98,4 @@ if [[ "$ins_ispconfig" == "true" ]];                    then
 							create_ispconfig_configuration
 				   			install_PureFTPD; install_Fail2BanDovecot; install_Fail2BanRulesDovecot; install_ISPConfig
 fi
-rm result
+rm results
